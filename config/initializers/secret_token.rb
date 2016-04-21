de0fe5982c1a14e8d8fb412854ac585a497a1fbb
Justin Martin <lupusdei108@gmail.com>
2016-04-21 17:56:21 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nexpil::Application.config.secret_key_base = 'a91a35c05ef67fd24896278fa417b6552475ec22722502f3aa09822f7fb25963d2336777d162fe3177dd4598f30570b3e8e327ef7a4ae6f62f5016ad4956606a'
