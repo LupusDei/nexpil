60fc2297ee0931cca6415374e32069649d4aaa2f
Justin Martin <lupusdei108@gmail.com>
2016-05-01 16:44:31 -0500

    Began adimin page for physicians

---
+++ app/admin/physician/patient.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User, namespace: Physician do
+  menu priority: 1, label: "Patient"
+  config.comments = false
+  index do
+      selectable_column
+      id_column
+      column :first_name
+      column :last_name
+      column :email
+      column :age
+      column :gender
+      column :comorbid_conditions
+
+      column :created_at
+      actions
+    end
+
+end
