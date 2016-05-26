dbd1f74e2172596f512c2bc7819d7678a960ee24
Justin Martin <lupusdei108@gmail.com>
2016-05-26 18:36:54 -0500

    Added Twilio sms service

---
+++ app/models/phone_service.rb
@@ -0,0 +1,18 @@
+class PhoneService
+
+  def send_message(to_number, message)
+    auth_token = SECRET_KEYS["twilio_secret_key"]
+    account_sid = SECRET_KEYS["twilio_api_key"]
+    saved_number = SECRET_KEYS["twilio_phone_number"]
+
+    begin
+      client = Twilio::REST::Client.new account_sid, auth_token
+
+      options = {to: to_number, body: message, from: saved_number}
+      sms = client.account.messages.create(options)
+    rescue => e
+      #notify someone important that the message failed to go through
+    end
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+SECRET_KEYS = YAML.load_file(File.expand_path('../application.yml', __FILE__))
 module Nexpil
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
